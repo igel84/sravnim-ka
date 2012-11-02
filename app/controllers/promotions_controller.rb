0a420a842d1130c9d16525056c0eb0edfb840d2e
igel <igel.yulian@mail.ru>
2012-11-02 09:39:05 +0400

    add permission promotion

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -1,5 +1,6 @@
 class PromotionsController < ApplicationController
   before_filter :require_login
+  before_filter :require_admin
 
   def index
     @promotions = current_user.admin? == true ? Promotion.all : current_user.promotions
@@ -8,6 +9,9 @@ class PromotionsController < ApplicationController
   def create
     if params[:promotion][:id]
       @promotion = Promotion.find(params[:promotion][:id])
+      params[:promotion][:info] = @promotion.info
+      params[:promotion][:actice] = @promotion.active            
+      params[:promotion][:balance] = @promotion.balance
       @promotion.update_attributes(params[:promotion])
     else
       params[:promotion][:user_id] = current_user.id
@@ -22,6 +26,8 @@ class PromotionsController < ApplicationController
   def update
     @promotion = Promotion.find(params[:id])
     params[:promotion][:balance] = params[:promotion][:balance].to_i * 10
+    params[:promotion][:info] = @promotion.info
+    params[:promotion][:actice] = @promotion.active                
     @promotion.update_attributes(params[:promotion])
     respond_to do |format|
       format.html { redirect_to promotions_path }
@@ -44,4 +50,9 @@ class PromotionsController < ApplicationController
     redirect_to promotions_path
   end
 
+  private
+    def require_admin
+      params[:promotion][:actice] = nil if params[:promotion][:actice] && !current_user.admin?
+      params[:promotion][:balance] = nil if params[:promotion][:balance] && !current_user.admin?      
+    end
 end
